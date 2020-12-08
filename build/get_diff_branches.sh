66edefb5cdab414f4fd1ddfd708a7a90bc9a0af1
u1074663 <nagendra.singh@iqvia.com>
2020-12-08 08:34:15 +0530

    Test me

--- build/get_diff_branches.sh
+++ build/get_diff_branches.sh
@@ -4,10 +4,10 @@ echo $ghprbTargetBranch
 
 echo 'Starting git diff'
 cd ..
-echo 'Switching to origin/'$ghprbTargetBranch
-git checkout -f origin/$ghprbTargetBranch
-echo 'Merging target branch (origin/'$1') into current branch (origin/'$ghprbTargetBranch')'
-git merge --no-commit --no-ff origin/$1
+echo 'Switching to origin/master'
+git checkout -f origin/master
+echo 'Merging target branch (origin/CEAS-1234) into current branch (origin/master)'
+git merge --no-commit --no-ff origin/CEAS-1234
 echo 'Merged. Looking for conflicts'
 git ls-files -u | awk '{$1=$2=$3=""; print $0}' | awk '{ sub(/^[ \t]+/, ""); print }' | sort -u > conflicts.txt
 if [ -s conflicts.txt ]
@@ -19,7 +19,7 @@ then
     exit 1
 else
     echo 'Starting git diff'
-    git diff origin/$ghprbTargetBranch origin/$1 > diffPatch.patch
+    git diff origin/master origin/CEAS-1234 > diffPatch.patch
     git status -s > diff.txt
     echo 'Git diff done.'
     echo 'Clean double quotes'
